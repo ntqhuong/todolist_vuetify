7d0e377b9cb70d0adc4b2339a36de86f04ddcd17
Nguyễn Thị Quỳnh Hương <iris.nguyen1309.qh@gmail.com>
2020-10-28 08:13:19 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -29,69 +29,6 @@ export default {
   components: {
     TodoList,
   },
-
-  data() {
-    return {
-      isDark: true,
-      show: true,
-      newTodo: "",
-      todos: [],
-      day: this.todoDay(),
-      date: new Date().getDate(),
-      ord: this.nth(new Date().getDate()),
-      year: new Date().getFullYear(),
-    };
-  },
-  methods: {
-    addTodo() {
-      var value = this.newTodo && this.newTodo.trim();
-      if (!value) {
-        return;
-      }
-
-      this.todos.push({
-        title: this.newTodo,
-        done: false,
-      });
-      this.newTodo = "";
-    },
-    removeTodo(index) {
-      this.todos.splice(index, 1);
-    },
-    todoDay() {
-      var d = new Date();
-      var days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ];
-      return days[d.getDay()];
-    },
-    nth(d) {
-      if (d > 3 && d < 21) return "th";
-      switch (d % 10) {
-        case 1:
-          return "st";
-        case 2:
-          return "nd";
-        case 3:
-          return "rd";
-        default:
-          return "th";
-      }
-    },
-  },
-  filters: {
-    capitalize: function(value) {
-      if (!value) return "";
-      value = value.toString();
-      return value.charAt(0).toUpperCase() + value.slice(1);
-    },
-  },
 };
 </script>
 <style>
