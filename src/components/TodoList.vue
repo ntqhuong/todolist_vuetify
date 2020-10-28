44b820b2baa688d0e9037de2ae310746cb78b8c4
phamngocminh <ngocp1975@gmail.com>
2020-10-28 23:25:24 +0700

    sass

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,102 +1,98 @@
 <template>
-<v-card>
-
-  <v-data-table
-    :headers="headers"
-    :items="desserts"
-    :search="search"
-    sort-by="calories"
-    class="elevation-1"
-  >
-    <template v-slot:top>
-      <v-toolbar flat>
-        <v-toolbar-title>Todo List</v-toolbar-title>
-        <v-divider class="mx-4" inset vertical></v-divider>
-              <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
-        <v-dialog v-model="dialog" max-width="500px">
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn color="warning" dark class="mb-2" v-bind="attrs" v-on="on">
-              New Item
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="headline">{{ formTitle }}</span>
-            </v-card-title>
-
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.description"
-                      label="Description"
-                    ></v-text-field>
-                  </v-col>
-                </v-row> 
-              </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="secondary" @click="close">
-                Cancel
+  <v-card>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      @click="toggleTodo(desserts)"
+      :search="search"
+      sort-by="calories"
+      class="elevation-1"
+    >
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>Todo List</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+          <v-dialog v-model="dialog" max-width="500px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="warning" dark class="mb-2" v-bind="attrs" v-on="on">
+                New Item
               </v-btn>
-              <v-btn color="blue"  @click="save">
-                Save
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-dialog v-model="dialogDelete" max-width="500px">
-          <v-card>
-            <v-card-title class="headline"
-              >Are you sure you want to delete this item?</v-card-title
-            >
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="secondary" @click="closeDelete"
-                >Cancel</v-btn
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">{{ formTitle }}</span>
+              </v-card-title>
+
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.description"
+                        label="Description"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="secondary" @click="close">
+                  Cancel
+                </v-btn>
+                <v-btn color="blue" @click="save">
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+              <v-card-title class="headline"
+                >Are you sure you want to delete this item?</v-card-title
               >
-              <v-btn color="blue" @click="deleteItemConfirm"
-                >OK</v-btn
-              >
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-toolbar>
-    </template>
-    <template v-slot:item.actions="{ item }">
-      <v-btn color="red" small class="mr-2" @click="editItem(item)">
-        Edit
-      </v-btn>
-      <v-btn color="success" small @click="deleteItem(item)">
-        Delete
-      </v-btn>
-    </template>
-    <template v-slot:no-data>
-      <v-btn color="primary" @click="initialize">
-        Reset
-      </v-btn>
-    </template>
-  </v-data-table>
-   </v-card>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="secondary" @click="closeDelete">Cancel</v-btn>
+                <v-btn color="blue" @click="deleteItemConfirm">OK</v-btn>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <v-btn color="red" small class="mr-2" @click="editItem(item)">
+          Edit
+        </v-btn>
+        <v-btn color="success" small @click="deleteItem(item)">
+          Delete
+        </v-btn>
+      </template>
+      <template v-slot:no-data>
+        <v-btn color="primary" @click="initialize">
+          Reset
+        </v-btn>
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 <script>
 export default {
   data: () => ({
     dialog: false,
     dialogDelete: false,
-    search: '',
+    search: "",
     headers: [
-      {text: "Description",align: "start",value: "description"},
+      { text: "Description", align: "start", value: "description" },
       { text: "Actions", value: "actions", sortable: false }
     ],
     desserts: [],
@@ -105,7 +101,7 @@ export default {
       description: ""
     },
     defaultItem: {
-    description: ""
+      description: ""
     }
   }),
 
@@ -133,17 +129,21 @@ export default {
       this.desserts = [
         {
           description: "Do the dishes",
+          completed: false
         },
         {
           description: "Take out the trash",
+          completed: false
         },
         {
           description: "Finish doing laundry",
-        },
-       
+          completed: false
+        }
       ];
     },
-
+    toggleTodo(desserts) {
+      desserts.completed = !desserts.completed;
+    },
     editItem(item) {
       this.editedIndex = this.desserts.indexOf(item);
       this.editedItem = Object.assign({}, item);
@@ -188,22 +188,21 @@ export default {
   }
 };
 </script>
-<style>
-.v-main__wrap {
 
-    margin: 100px;
-    margin-top: 40px;
 
-}
-.theme--light.v-data-table {
-    background-color: rgb(248, 227, 200);
-}
-.v-toolbar__content, .v-toolbar__extension {
-    background-color: indianred;
-    border-radius: 5px;
-}
-.v-input--hide-details > .v-input__control > .v-input__slot {
-    width: 95%;
-    margin-bottom: 8px;
-}
+<style lang="sass">
+.v-main__wrap 
+  margin: 100px
+  margin-top: 40px
+.theme--light 
+  &.v-data-table 
+    background-color: rgb(248, 227, 200)
+.v-toolbar__content, .v-toolbar__extension 
+  background-color: indianred
+  border-radius: 5px
+.v-input--hide-details 
+  & > .v-input__control 
+    & > .v-input__slot 
+      width: 95%
+      margin-bottom: 8px
 </style>
